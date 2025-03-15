aa2fc2f0ea89c83ff9a050cb85bd2a7ec549e54b
jtebbe <jtebbe@umich.edu>
2025-03-15 13:47:02 -0500

    Remove pickup exclusives and change bag size

--- include/config/save.h
+++ include/config/save.h
@@ -2,10 +2,10 @@
 #define GUARD_CONFIG_SAVE_H
 
 // SaveBlock1 configs
-#define FREE_EXTRA_SEEN_FLAGS_SAVEBLOCK1    FALSE   // Free up unused Pokédex seen flags (52 bytes).
-#define FREE_TRAINER_HILL                   FALSE   // Frees up Trainer Hill data (28 bytes).
-#define FREE_MYSTERY_EVENT_BUFFERS          FALSE   // Frees up ramScript (1104 bytes).
-#define FREE_MATCH_CALL                     FALSE   // Frees up match call and rematch / VS Seeker data. (104 bytes).
+#define FREE_EXTRA_SEEN_FLAGS_SAVEBLOCK1    TRUE   // Free up unused Pokédex seen flags (52 bytes).
+#define FREE_TRAINER_HILL                   TRUE   // Frees up Trainer Hill data (28 bytes).
+#define FREE_MYSTERY_EVENT_BUFFERS          TRUE   // Frees up ramScript (1104 bytes).
+#define FREE_MATCH_CALL                     TRUE   // Frees up match call and rematch / VS Seeker data. (104 bytes).
 #define FREE_UNION_ROOM_CHAT                TRUE   // Frees up union room chat (212 bytes).
 #define FREE_ENIGMA_BERRY                   TRUE   // Frees up E-Reader Enigma Berry data (52 bytes).
 #define FREE_LINK_BATTLE_RECORDS            TRUE   // Frees up link battle record data (88 bytes).
